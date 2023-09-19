71b88a83efb6d0fa5c5e680b8fcc822c14d7c698
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-09-19 17:58:06 +0530

    new:added the hero section for the home page

---
+++ src/components/HeroHome.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const HeroHome = () => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-auto bg-black text-white">
+      <div className="flex flex-col justify-center items-center h-auto md:w-[60%] sm:w-[50%] w-[90%] gap-10 py-20">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="lg:text-2xl md:text-xl sm:text-lg text-base font-semibold">
+            One step in the right direction
+          </h1>
+        </div>
+        <div className="flex flex-col justify-center items-center gap-5 md:w-[80%]">
+          <h1 className="lg:text-5xl md:text-3xl sm:text-2xl text-lg font-semibold">
+            Bag a job. Escalate your career.
+          </h1>
+          <p className="lg:text-xl md:text-lg sm:text-base text-sm text-center">
+            All you need from a job is to be rewarding and add value to your
+            career timeline, isn{"’"}t it? We get it. That{"’"}s why we believe
+            in creating a working environment that caters to your goals.
+            Starting from the process of finding the right role to having you
+            look forward to coming into work every day, we make every step super
+            smooth. Browse available jobs now!
+          </p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <button type="submit" className="px-7 text-lg font-semibold capitalize py-3 cursor-pointer shadow-md hover:shadow-lg transition-all duration-300 ease-in-out bg-blue-600 hover:bg-blue-700 rounded-[12px]">View Openings</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroHome;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,67 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import HeroHome from "../components/HeroHome";
 
 const HomePage = () => {
   return (
     <>
       <Header />
-      <div className="bg-black h-[200vh] text-white">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam et iste
-        sed commodi eum itaque incidunt, harum doloremque earum illum repellat
-        beatae tenetur saepe dignissimos voluptatum officiis fuga dolore
-        recusandae alias corporis perspiciatis modi rem possimus! Sit repellat
-        molestiae quam soluta, ea quod? Accusamus sequi soluta animi quasi
-        asperiores omnis, accusantium labore provident dolorem distinctio et
-        adipisci nulla magni, rerum repellendus aliquid minima velit! Voluptate
-        praesentium natus ea molestias ut voluptas? Asperiores delectus dolorum
-        earum nulla illo tempora eos. Recusandae fugiat eveniet, fugit porro eos
-        aspernatur maiores quo voluptatum illo totam optio, assumenda
-        necessitatibus quis possimus ipsam doloremque quibusdam aut pariatur
-        fuga commodi et earum laboriosam perspiciatis eius! Placeat excepturi
-        totam nobis eaque vero, eligendi accusantium, iste qui possimus
-        explicabo error numquam, ipsum aliquam iusto recusandae vitae? Fugit rem
-        quaerat in impedit magni consectetur aut nulla sit odio, eligendi id
-        commodi. Quaerat dolor mollitia quae delectus, omnis nulla architecto
-        tenetur ipsam sequi fuga assumenda. Debitis accusamus sit, quia mollitia
-        pariatur et cumque laborum rerum inventore, in consectetur blanditiis
-        doloribus, magni maxime officiis voluptas corrupti est ratione? Quisquam
-        facilis ullam, repellendus, nisi itaque assumenda voluptas labore
-        voluptatum natus, rem accusantium incidunt architecto facere vel impedit
-        ab harum eligendi voluptate odio et tempore. Porro facilis nobis ducimus
-        aut earum. Quos impedit nostrum soluta recusandae sapiente aperiam dolor
-        commodi laudantium repudiandae magnam? Iure ex placeat consequuntur
-        fugit sequi rem libero dolore quis? Et veritatis dolorem hic dolores,
-        repellendus, amet, excepturi nostrum inventore esse omnis dolorum!
-        Deleniti numquam facere dicta. At perspiciatis ipsam assumenda ad eum
-        delectus, nesciunt iste ipsum magnam incidunt totam necessitatibus modi
-        ducimus illum voluptates tenetur adipisci? Cum, labore obcaecati
-        temporibus beatae, sint porro alias ea totam quisquam voluptatum ut
-        voluptas provident sequi reiciendis voluptatibus nisi fugit doloribus,
-        placeat deleniti explicabo eius. Placeat minus non id quisquam
-        consequuntur suscipit nesciunt fugiat voluptate nemo deleniti eaque,
-        ducimus, in consectetur, aperiam repellat. Modi optio perferendis
-        architecto eum repellendus neque voluptate nulla aspernatur sapiente
-        nam? Soluta voluptates sunt voluptas exercitationem quisquam.
-        Distinctio, magni consequatur? Quis quasi libero iusto repellat quaerat
-        at nemo repellendus voluptates ipsa dolores officiis, perferendis
-        deleniti ipsum quos illum quibusdam! Quos velit neque eaque eveniet
-        illum vero quidem fugiat repellendus nulla animi dignissimos voluptatum
-        quae eius similique minima unde, modi ratione dolores. Sunt, commodi
-        officiis nulla sint at enim omnis eum eligendi, illo atque nesciunt quae
-        harum. Dicta recusandae perspiciatis quibusdam! Incidunt repellendus non
-        eius voluptates? Inventore, quae tempora? Eum deserunt neque aliquid,
-        molestiae harum rem recusandae. Error ex aliquid cumque iusto itaque.
-        Eligendi aspernatur nisi debitis iusto commodi animi illo, omnis
-        laboriosam voluptatibus odit officia, inventore hic mollitia dolore
-        voluptate sequi quos praesentium aperiam enim labore dicta architecto?
-        Ex placeat, sint perferendis minima in dolorum voluptatum aut mollitia
-        minus nesciunt iure enim porro corporis. Perferendis, aliquid quasi.
-        Harum, obcaecati. Ex atque rerum perspiciatis harum quaerat, tenetur
-        cupiditate similique, ab, hic excepturi rem eveniet aspernatur nesciunt
-        voluptatum quisquam sed. Veniam nemo ipsam obcaecati cum quae non
-        exercitationem iure, debitis velit voluptatibus dolor pariatur commodi
-        deserunt accusamus officiis reiciendis rerum esse eum.
-      </div>
+      <HeroHome />
+      <Footer/>
     </>
   );
 };
