6ee8c44ea9c046f1ecfd02e163da177a6b935cdd
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-09-24 13:11:37 +0530

    new:added the info model

---
+++ server/models/applicationInformation.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const applicationInfoSchema = mongoose.Schema({
+  firstName: {
+    required: true,
+    type: String,
+  },
+  lastName: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+  },
+  mobile: {
+    required: true,
+    type: String,
+  },
+  street: {
+    type: String,
+    default: "",
+  },
+  city: {
+    type: String,
+    default: "",
+  },
+  state: {
+    type: String,
+    default: "",
+  },
+  zip: {
+    type: String,
+    default: "",
+  },
+  country: {
+    type: String,
+    default: "",
+  },
+  linkedIn: {
+    type: String,
+    required: true,
+  },
+  github: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  resume: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Application", applicationInfoSchema);
