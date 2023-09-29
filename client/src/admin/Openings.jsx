8f2fb4b3aaaad955de298dd0c5966de02012e745
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-09-29 18:42:47 +0530

    removed files

--- client/src/admin/Openings.jsx
+++ client/src/admin/Openings.jsx
@@ -1,57 +1,142 @@
 import React, { useState } from "react";
-import ParentCheckbox from "./ParentCheckBox";
-import { teams } from "../data/data";
+import {
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Typography,
+  Button,
+} from "@mui/material";
 
-const Openings = () => {
-  const [selectedData, setSelectedData] = useState([]);
+const ParentCheckbox = ({
+  parentLabel,
+  childrenLabels,
+  onParentChange,
+  onChildChange,
+}) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const [selectedChildren, setSelectedChildren] = useState([]);
 
-  const handleCheckboxChange = (name, isChecked, isParent) => {
-    if (isParent) {
-      // const childrenNames = teams
-      //   .find((group) => group.name === name)
-      //   .positions.map((child) => `${name}-${child.name}`);
-      // if (isChecked) {
-      //   setSelectedData([...selectedData, ...childrenNames]);
-      // } else {
-      //   setSelectedData(
-      //     selectedData.filter((item) => !childrenNames.includes(item))
-      //   );
-      // }
-    } else {
-      if (isChecked) {
-        console.log("iuwegdhjg")
-        setSelectedData([...selectedData, name]);
-      } else {
-        setSelectedData(selectedData.filter((item) => item !== name));
-      }
+  const handleParentChange = (event) => {
+    const isChecked = event.target.checked;
+    setIsChecked(isChecked);
+    onParentChange(parentLabel, isChecked);
+
+    if (!isChecked) {
+      setSelectedChildren([]);
     }
+  };
+
+  const handleChildChange = (childLabel, isChecked) => {
+    const updatedSelectedChildren = isChecked
+      ? [...selectedChildren, childLabel]
+      : selectedChildren.filter((label) => label !== childLabel);
+
+    setSelectedChildren(updatedSelectedChildren);
+    onChildChange(parentLabel, updatedSelectedChildren);
   };
 
   return (
     <div>
-      {teams.map((group, index) => (
-        <ParentCheckbox
-          key={index}
-          name={group.name}
-          children={group.positions}
-          onCheckboxChange={(name, isChecked) =>
-            handleCheckboxChange(name, isChecked, true)
-          }
-        />
-      ))}
-      <div>
-        <h3>Selected Data:</h3>
-        <ul>
-          {selectedData.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </div>
-      <button onClick={() => console.log(selectedData)}>
-        Print Selected Data
-      </button>
+      <FormControlLabel
+        control={<Checkbox checked={isChecked} onChange={handleParentChange} />}
+        label={parentLabel}
+      />
+      <Grid container>
+        {childrenLabels.map((childLabel, index) => (
+          <Grid item xs={4} key={index}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  disabled={!isChecked}
+                  checked={selectedChildren.includes(childLabel)}
+                  onChange={(event) =>
+                    handleChildChange(childLabel, event.target.checked)
+                  }
+                />
+              }
+              label={childLabel}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
 
-export default Openings;
+const CheckboxComponent = () => {
+  const [selectedParents, setSelectedParents] = useState([]);
+  const [selectedChildren, setSelectedChildren] = useState([]);
+
+  const handleParentChange = (parentLabel, isChecked) => {
+    const updatedSelectedParents = isChecked
+      ? [...selectedParents, { name: parentLabel, positions: [] }]
+      : selectedParents.filter((label) => label !== parentLabel);
+
+    setSelectedParents(updatedSelectedParents);
+  };
+
+  const handleChildChange = (parentLabel, selectedChildren) => {
+    // const updatedSelectedChildren = [...selectedChildren];
+
+    // .findIndex(obj => obj.name.includes(parentLabel));
+
+    setSelectedChildren(updatedSelectedChildren);
+  };
+
+  const parentLabels = [
+    "Parent 1",
+    "Parent 2",
+    "Parent 3",
+    "Parent 4",
+    "Parent 5",
+    "Parent 6",
+    "Parent 7",
+    "Parent 8",
+    "Parent 9",
+  ];
+  const childrenLabels = ["Child 1", "Child 2", "Child 3"];
+
+  const handleSaveState = () => {
+    const savedState = selectedParents.map((parent) => {
+      const selectedChildrenForParent = selectedChildren.filter((child) =>
+        child.startsWith(parent)
+      );
+      return {
+        parent,
+        selectedChildren: selectedChildrenForParent,
+      };
+    });
+    console.log(savedState);
+  };
+
+  const selectedData = {
+    selectedParents: selectedParents,
+    selectedChildren: selectedChildren,
+  };
+
+  return (
+    <div>
+      <Typography variant="h6" gutterBottom>
+        Parent Checkboxes
+      </Typography>
+      {parentLabels.map((label, index) => (
+        <ParentCheckbox
+          key={index}
+          parentLabel={label}
+          childrenLabels={childrenLabels}
+          onParentChange={handleParentChange}
+          onChildChange={handleChildChange}
+        />
+      ))}
+      <Typography variant="h6" gutterBottom>
+        Selected Data
+      </Typography>
+      <pre>{JSON.stringify(selectedData, null, 2)}</pre>
+      <Button variant="contained" color="primary" onClick={handleSaveState}>
+        Save the State
+      </Button>
+    </div>
+  );
+};
+
+export default CheckboxComponent;
