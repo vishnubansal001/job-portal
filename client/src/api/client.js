e1666b10b9f2dabc028cc298605cea27353ce8c2
Yakshit <92084737+YAKSHIT-22@users.noreply.github.com>
2023-10-01 14:53:48 +0530

    Update client.js

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const client = axios.create({ baseURL: "http://localhost:8000" });
+const client = axios.create({ baseURL: "http://job-portal-server-seven-xi.vercel.app" });
 
 export default client;
