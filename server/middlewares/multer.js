8229f66273bed23818914665266718c3b31e5d2d
Yakshit <92084737+YAKSHIT-22@users.noreply.github.com>
2023-10-11 16:51:53 +0530

    Update multer.js

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "/tmp/");
   },
   filename: function (req, file, cb) {
     if (file.mimetype.startsWith("image/")) {
