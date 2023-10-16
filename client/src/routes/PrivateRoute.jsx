77d0694b5ccce70874a1466fe900c4b0b57e980c
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-10-16 10:47:39 +0530

    the private route functionality setup

--- client/src/routes/PrivateRoute.jsx
+++ client/src/routes/PrivateRoute.jsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../hooks";
 
 const PrivateRoute = () => {
-  const [loggedIn, setLoggedIn] = useState(true);
+  const { authInfo } = useAuth();
+  const [loggedIn, setLoggedIn] = useState(authInfo.isLoggedIn);
+  useEffect(()=>{
+    setLoggedIn(authInfo.isLoggedIn)
+  },[authInfo.isLoggedIn])
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 };
 
